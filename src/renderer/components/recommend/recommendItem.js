bee8622840555e5f5b0eb07acd6d2dc3b95ec027
aa <398561930@qq.com>
2021-06-03 23:44:25 +0800

    add:推荐歌单

---
+++ src/renderer/components/recommend/recommendItem.js
@@ -0,0 +1,33 @@
+import homeApi from "../../../apis/home.api/home.api";
+export default {
+    data() {
+        return {
+            limit: 10,
+            list: [],
+
+        }
+    },
+    methods: {
+        showImgs(item) {
+            item.isShowIcon = true;
+            this.$refs.musicBox.style = "filter: blur(2px);";
+        },
+        hiddenImgs(item) {
+            item.isShowIcon = false;
+            this.$refs.musicBox.style = "filter: blur(0);";
+        },
+        details() { },
+        //获取推荐歌单
+        async getRecommend() {
+            const { data: res } = await homeApi.getRecommend(this.limit);
+            res.result.forEach(item=>{
+                item['isShowIcon'] = false;
+            })
+            this.list = res.result;
+            // console.log(res);
+        },
+    },
+    created() {
+        this.getRecommend();
+    }
+}
---
+++ src/renderer/components/recommend/recommendItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="recommend">
+    <div v-for="item in list">
+      <div class="item">
+        <div
+          ref="musicBox"
+          class="music"
+          @mouseenter="showImgs(item)"
+          @mouseleave="hiddenImgs(item)"
+          @click="details"
+        >
+          <img :src="item.picUrl" alt="歌单封面" />
+          <div class="btnImg" :style="{ opacity: item.isShowIcon ? 1 : 0 }">
+            <i class="iconfont icon-play"></i>
+          </div>
+          <div
+            :class="[
+              'content-copy-writer',
+              item.isShowIcon ? 'downAimation' : 'upAnimation',
+            ]"
+          >
+            <div>{{ item.copywriter }}</div>
+          </div>
+        </div>
+
+        <span @click="details">{{ item.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import recommendJS from "./recommendItem";
+export default {
+  ...recommendJS,
+};
+</script>
+<style lang="scss" scoped>
+@import "./recommendItem";
+@import url("//at.alicdn.com/t/font_2585248_z7kzr75ewi.css");
+</style>
